cde6f01e87972ed22357b533d046ddb6346d1b0e
alejandronavarromenzel-dev <alejandronavarromenzel@gmail.com>
2026-01-29 15:53:35 -0300

    Update task.go

--- src/internal/models/task.go
+++ src/internal/models/task.go
@@ -1,15 +1,12 @@
 package models
 
-import "time"
-
 type Task struct {
-	ID        int64
-	ClientID  int64
-	Title     string
-	Status    string
-	Priority  string
-	Owner     string
-	Progress  int
-	DueDate   *time.Time
-	CreatedAt time.Time
+	ID         int64
+	ClientID   int64
+	ClientName string
+
+	Title    string
+	Status   string
+	Priority string
+	Progress int
 }
